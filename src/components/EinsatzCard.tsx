e8ab150eaccc8c9aea276ac6ff53fa5d0977e8d3
Alessandro Orlandi <aorlandi1990@gmail.com>
2025-03-02 19:56:19 +0100

    fix: text overflow handling

--- src/components/EinsatzCard.tsx
+++ src/components/EinsatzCard.tsx
@@ -16,41 +16,78 @@ export const EinsatzCard: React.FC<Props> = ({ einsatz }) => {
     const isMobile = useResponsive('down', 'md')
 
     return (
-        <Card sx={{ display: "flex", width: isMobile ? "100%" : "50%", borderRadius: 3, overflow: "hidden", boxShadow: 3, borderWidth: 0.5, borderColor: "lightgrey", borderStyle: "solid" }}>
+        <Card sx={{
+            display: "flex",
+            flex: 1,
+            borderRadius: 3,
+            overflow: "hidden",
+            boxShadow: 3,
+            borderWidth: 0.5,
+            borderColor: "lightgrey",
+            borderStyle: "solid",
+            minWidth: 0
+        }}>
             <CardContent sx={{
-                flex: 1, display: "flex", flexDirection: "row", gap: 2, alignItems: "flex-start", paddingTop: 2, paddingBottom: 2, paddingRight: 2, paddingLeft: 2
+                display: "flex",
+                flexDirection: "row",
+                gap: 2,
+                padding: 2,
+                flex: 1,
+                minWidth: 0
             }}>
-
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 1, width: 80 }}>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 0.5,
+                    width: 80,
+                    flex: 'none',
+                }}>
                     <Label color={mapKeywordToTextColor(einsatz.KAT)} backgroundColor={mapKeywordToColorSchema(einsatz.KAT)}>
                         {mapKeyword(einsatz.KAT)}
                     </Label>
-                    <Box display="flex" alignItems="center" gap={0.5}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
                         <NumbersIcon fontSize="small" />
                         <Typography variant="body1">{parseInt(einsatz.NR?.toString().split('-')[1]).toString()}</Typography>
                     </Box>
                     <Typography variant="body1">{dayjs(einsatz.VON).format("DD.MM.YY")}</Typography>
                 </Box>
 
-                <Divider orientation="vertical" flexItem sx={{ color: "black", backgroundColor: "black", opacity: 0.3 }} />
+                <Divider orientation="vertical" flexItem />
 
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-                    <Stack direction="column" spacing={0}>
-                        <Typography variant="subtitle1" fontWeight="bold">
-                            {einsatz.ART}
-                        </Typography>
-                    </Stack>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    flex: 1,
+                    gap: 2,
+                    minWidth: 0,
+                    width: '100%'
+                }}>
+                    <Typography
+                        noWrap
+                        variant="body1"
+                        sx={{
+                            width: '100%',
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            whiteSpace: "nowrap"
+                        }}
+                    >
+                        {einsatz.ART}
+                    </Typography>
 
-                    <Box display="flex" alignItems="flex-start" gap={1}>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: 0.5 }}>
                         <AccessTimeIcon fontSize="small" />
                         <Box>
-                            <Typography variant="body1">{dayjs(einsatz.VON, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')} Uhr</Typography>
-                            <Typography variant="body1" fontWeight="light">ca. {formatDuration(einsatz.VON, einsatz.BIS)}</Typography>
+                            <Typography variant="body1">
+                                {dayjs(einsatz.VON, "YYYY-MM-DD HH:mm:ss").format("HH:mm")} Uhr
+                            </Typography>
+                            <Typography variant="body1" fontWeight="light">
+                                ca. {formatDuration(einsatz.VON, einsatz.BIS)}
+                            </Typography>
                         </Box>
                     </Box>
                 </Box>
             </CardContent>
         </Card >
-
     )
 }
